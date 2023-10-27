40b9149e739de229514334826f53a39de1a7a227
Vitor Hugo Nascimento <115762112+vitorhugozj@users.noreply.github.com>
2023-10-27 15:47:05 -0300

    Update bcd08 - Sistema de Ensino.sql

--- bcd08 - Sistema de Ensino.sql
+++ bcd08 - Sistema de Ensino.sql
@@ -1,119 +1,168 @@
-create database sistema_educacional;
+create database sistema_educacional; 
+
 use sistema_educacional;
 
-create table cadastrar_aluno
+create table alunos
 (
 id_aluno integer primary key,
-nome varchar (100) not null,
+nome varchar(100) not null,
 cpf numeric (11) not null,
-telefone numeric (11) not null,
-email varchar (30) not null,
-data_nasc date,
-frequencia_aluno numeric (2)
+data_nascimento date,
+email varchar(100) not null,
+telefone numeric (13) not null,
+id_matricula int,
+
+foreign key (id_matricula) references matricula (id_matricula)
 );
 
-INSERT INTO cadastrar_aluno (id_aluno, nome, cpf, telefone, email, data_nasc, frequencia_aluno)
-VALUES
-(1, 'João', 11111111111, 11992267738, 'joaopimenta@outlook.com', '2000-04-05', 88),
-(2, 'José', 22222222222, 11998767384, 'joselarceda@outlook.com', '2002-02-23', 99),
-(3, 'Bianca', 33333333333, 11947227381, 'biancapinheiros@outlook.com', '2001-11-28', 96),
-(4, 'Guilherme', 44444444444, 11995228892, 'guiguiEM@outlook.com', '2003-11-02', 94),
-(5, 'Gustavo', 55555555555, 11997654384, 'gustaalmeida@outlook.com', '2002-07-17', 96),
-(6, 'Alice', 66666666666, 11995289823, 'alicepedrosa@outlook.com', '2001-01-06', 88);
+insert into alunos(id_aluno, nome, cpf, data_nascimento, email, telefone) values
+(1, 'Vitor', 72945129448, '2007-03-16', 'vitorhugo@gmail.com', 5511991784265),
+(2, 'Ricardo', 29852581220, '2006-12-04', 'ricardo21@gmail.com', 5511250880175),
+(3, 'Juan', 07313843281, '2002-09-28', 'juanRB@gmail.com', 5511562569823);
 
-create table cadastrar_professor
+create table professores
 (
 id_professor integer primary key,
-nome varchar (100) not null,
+nome varchar(100) not null,
 cpf numeric (11) not null,
-matricula_professor numeric,
-telefone numeric(11),
-email varchar (30) not null,
-data_nasc date
+email varchar(100) not null,
+telefone numeric (13) not null
 );
 
-INSERT INTO cadastrar_professor (id_professor, nome, cpf, telefone, email, data_nasc, matricula_professor)
-VALUES
-(23, 'Adailton', 46626579217, 11992267738, 'adailton1@outlook.com', '1982-11-05', 67),
-(62, 'Leandro', 48621321093, 11998767384, 'leandro6020@outlook.com', '1967-02-09', 84),
-(84, 'Marcia', 13095721799, 11947227381, 'marciaribeiro@outlook.com', '1976-08-28', 102),
-(03, 'Elaine', 21754098665, 11995228892, 'elaineoliveira@outlook.com', '1964-12-22', 74),
-(09, 'Ana Maria', 89217548571, 11997654384, 'anamariaclaudete@outlook.com', '1988-11-21', 19),
-(92, 'Roberto', 51649481149, 11995289823, 'robertrichards@outlook.com', '1992-01-04', 43);
+insert into professores(id_professor, nome, cpf, email, telefone) values
+(1, 'Roberto', 19545837938, 'robertoafonso17@gmail.com', 5511927584551),
+(2, 'Maria', 39525016681, 'mariafernanda@gmail.com', 5511291664147),
+(3, 'Fabio', 73401442849, 'fabiogonzaga@gmail.com', 5511296157385);
 
-
-create table cursos 
+create table cursos
 (
 id_curso integer primary key,
-nome_curso varchar (20) not null,
-carga_horaria numeric (4) not null,
-categoria_curso varchar (100) not null,
+nome_curso varchar(100) not null,
+carga_horaria numeric not null,
+id_aluno integer
 
-id_professor integer,
-id_tipos_curso integer,
 
-foreign key (id_tipos_curso) references tipos_curso(id_tipos_curso),
-foreign key (id_professor) references cadastrar_professor(id_professor)
 );
 
-create table turmas 
-(
-id_turmas integer primary key,
-numero_sala numeric (2),
-qtd_alunos numeric (32),
+insert into cursos(id_curso, nome_curso, carga_horaria, id_aluno) values
+(1, 'Engenharia', 1570, 1),
+(2, 'Nutrição', 1225, 2),
+(3, 'Redes', 1100, 3);
 
+create table materias
+(
+id_materia integer primary key,
+nome_materia varchar(100) not null,
+carga_horaria numeric not null,
+id_curso integer,
+id_professor integer
+
+);
+
+
+
+insert into materias(id_materia, nome_materia, carga_horaria, id_curso, id_professor) values
+(1, 'Cálculo', 120, 1, 1),
+(2, 'Alimentos', 75, 2, 2),
+(3, 'Hardware', 100, 3, 3);
+
+create table turmas
+(
+id_turma integer primary key,
+codigo_turma varchar(100) not null,
+quantidade_alunos numeric not null,
+id_curso integer
+
+);
+
+insert into turmas(id_turma, codigo_turma, quantidade_alunos, id_curso) values
+(1, 'EG1AIT', 16, 1),
+(2, 'NT2CIT', 34, 2),
+(3, 'RD1BIT', 22, 3);
+
+create table matriculas
+(
+id_matricula integer primary key,
+data_matricula date,
 id_aluno integer,
-id_professor integer,
+id_curso integer
 
-foreign key (id_aluno) references cadastrar_aluno(id_aluno),
-foreign key (id_professor) references cadastrar_professor(id_professor)
 );
 
-create table disciplinas
+insert into matriculas(id_matricula, data_matricula, id_aluno, id_curso) values
+(1, '2021-01-25', 1, 1),
+(2, '2019-07-02', 2, 2),
+(3, '2023-01-24', 3, 3);
+
+create table notas
 (
-disciplina_id integer primary key,
-nome_disciplina varchar (100) not null,
-codigo_disciplina varchar (100) not null,
-carga_horaria int
+id_nota integer primary key,
+nota float,
+data_avaliacao date,
+id_aluno integer,
+id_turma integer
+
+
 );
 
-create table eventos_academicos
+insert into notas(id_nota, nota, data_avaliacao, id_aluno, id_turma) values
+(1, 85, '2023-11-07', 1, 1),
+(2, 50, '2021-01-22', 2, 2),
+(3, 90, '2022-06-29', 3, 3);
+
+create table presenca
+(
+id_presenca integer primary key,
+data_aula date,
+status_aluno varchar(100) not null,
+id_aluno integer,
+id_turma integer
+
+);
+
+insert into presenca(id_presenca, data_aula, status_aluno, id_aluno, id_turma) values
+(1, '2023-04-20', 'Presente', 1, 1),
+(2, '2021-05-05', 'Ausente', 2, 2),
+(3, '2022-06-10', 'Presente', 3, 3);
+
+create table eventos
 (
 id_evento integer primary key,
-nome_evento varchar (100) not null,
+tema_evento varchar(100) not null,
 data_evento date,
-tema_evento varchar (100) not null
+id_curso integer,
+id_turma integer
+
+
 );
 
-create table vagas_estagio
-(
-id_estagio integer primary key,
-nome_estagio varchar (100) not null,
-carga_horaria numeric (5),
-nome_empresa varchar (100) not null
-);
+insert into eventos(id_evento, tema_evento, data_evento, id_curso, id_turma) values
+(1, 'Palestra', '2023-12-19', 1, 1),
+(2, 'Teatro', '2020-02-10', 2, 2),
+(3, 'Reunião', '2022-12-11', 3, 3);
 
-create table projetos_empresas
-(
-id_projetos integer primary key,
-nome_projeto varchar (100) not null,
+-- 1
+select eventos.tema_evento, codigo_turma, cursos.nome_curso from eventos
+inner join turmas on eventos.id_turma = turmas.id_turma
+inner join cursos on turmas.id_curso = cursos.id_curso;
 
-id_turmas integer,
-foreign key (id_turmas)  references turmas(id_turmas)
-);
+-- 2
+select matriculas.id_matricula, alunos.nome, avg(notas.nota) as media, cursos.nome_curso from notas
+inner join alunos on notas.id_nota = alunos.id_aluno
+inner join matriculas on alunos.id_matricula = matriculas.id_aluno
+inner join cursos on matriculas.id_curso = cursos.id_curso
+group by alunos.nome;
 
-create table tipos_curso
-(
-id_tipos_curso integer primary key,
-tipo_curso varchar(50) not null
-);
+-- 3
+select alunos.nome, presenca.status_aluno, materias.nome_materia, cursos.nome_curso from alunos
+inner join presenca on alunos.id_presenca=presenca.id_aluno
+inner join materias on presenca.id_materia=materias.id_materia
+inner join cursos on materias.id_curso=cursos.id_curso;
 
-INSERT INTO tipos_curso (id_tipos_curso, tipo_curso)
-VALUES
-(),
-(),
-(),
-(),
-(),
-();
+-- 4 
+select alunos.nome, matriculas.id_matricula as codigo_aluno, matriculas.data_matricula, cursos.nome_curso from alunos
+inner join matriculas on alunos.id=matriculas.aluno_id
+inner join cursos on matriculas.curso_id=cursos.id;
+
+
 
